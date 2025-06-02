488d7271e5e4651ff43da6955c139ffb3ee444b8
Ginnie <kinga.ginn.renz@gmail.com>
2025-06-02 13:44:44 +0200

    missing coma

--- turbidity_driver/setup.py
+++ turbidity_driver/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'turbidity_driver = turbidity_driver.parse_turbidity:main'
+            'turbidity_driver = turbidity_driver.parse_turbidity:main',
         ],
     },
 )
